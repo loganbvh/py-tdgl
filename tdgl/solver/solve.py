409992a9feef683b6f8736ba1b175f0c90f49759
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2023-01-06 10:54:29 -0800

    Ensure that disorder_epsilon returns floats (#11)
    * Ensure that disorder_epsilon returns floats

--- tdgl/solver/solve.py
+++ tdgl/solver/solve.py
@@ -286,11 +286,11 @@ def solve(
     psi_init[normal_boundary_index] = 0
     mu_init = np.zeros(len(mesh.sites))
     mu_boundary = np.zeros_like(mesh.edge_mesh.boundary_edge_indices, dtype=float)
-    # Create the epsilon parameter, which is the maximum value of |psi| at each position.
+    # Create the epsilon parameter, which sets the local critical temperature.
     if callable(disorder_epsilon):
-        epsilon = np.apply_along_axis(disorder_epsilon, 1, sites).astype(float)
+        epsilon = np.array([float(disorder_epsilon(r)) for r in sites])
     else:
-        epsilon = disorder_epsilon * np.ones(len(sites))
+        epsilon = disorder_epsilon * np.ones(len(sites), dtype=float)
     if np.any(epsilon < -1) or np.any(epsilon > 1):
         raise ValueError("The disorder parameter epsilon must be in range [-1, 1].")
 
