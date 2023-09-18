2a816da080658c85bd18a74d39908b154ac8b585
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2023-09-18 15:34:47 -0700

    update

--- tdgl/solver/screening.py
+++ tdgl/solver/screening.py
@@ -58,15 +58,14 @@ if cupy is not None:
         edge_centers,
         A_induced,
     ):
-        i = cupyx.jit.grid(1)
-        for k in cupyx.jit.range(2):
-            tmp = 0.0
-            for j in cupyx.jit.range(sites.shape[0]):
-                dx = edge_centers[i, 0] - sites[j, 0]
-                dy = edge_centers[i, 1] - sites[j, 1]
-                dr = cupy.sqrt(dx * dx + dy * dy)
-                tmp += J_site[j, k] * site_areas[j] / dr
-            A_induced[i, k] = tmp
+        i, k = cupyx.jit.grid(2)
+        tmp = 0.0
+        for j in cupyx.jit.range(sites.shape[0]):
+            dx = edge_centers[i, 0] - sites[j, 0]
+            dy = edge_centers[i, 1] - sites[j, 1]
+            dr = cupy.sqrt(dx * dx + dy * dy)
+            tmp += J_site[j, k] * site_areas[j] / dr
+        A_induced[i, k] = tmp
 
     @cupyx.jit.rawkernel()
     def get_A_induced_component_cupy(
