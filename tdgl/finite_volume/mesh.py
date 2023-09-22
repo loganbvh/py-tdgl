Fix scaling issue in screening code (#37)
* Fix scaling issue in screening code

* Update docs

tdgl/finite_volume/mesh.py
@@ -196,9 +196,7 @@ class Mesh:
         """
         # Normalize the edge direction
         directions = self.edge_mesh.directions
-        normalized_directions = (
-            directions / np.linalg.norm(directions, axis=1)[:, np.newaxis]
-        )
+        normalized_directions = directions / self.edge_mesh.edge_lengths[:, np.newaxis]
         if vector:
             flux_x = quantity_on_edge * normalized_directions[:, 0]
             flux_y = quantity_on_edge * normalized_directions[:, 1]

tdgl/solver/solve.py
@@ -8,8 +8,8 @@ import numpy as np
 import scipy.sparse as sp
 
 try:
-    import jax
-    import jax.numpy as jnp
+    import jax  # type: ignore
+    import jax.numpy as jnp  # type: ignore
 except (ModuleNotFoundError, ImportError):
     jax = None
 
@@ -243,17 +243,20 @@ def solve(
     sites = device.points
     edges = mesh.edge_mesh.edges
     edge_directions = mesh.edge_mesh.directions
+    normalized_directions = edge_directions / mesh.edge_mesh.edge_lengths[:, np.newaxis]
     length_units = ureg(device.length_units)
     xi = device.coherence_length.magnitude
-    probe_points = device.probe_point_indices
     u = device.layer.u
     gamma = device.layer.gamma
     K0 = device.K0
+    A0 = device.A0
+    probe_points = device.probe_point_indices
     # The vector potential is evaluated on the mesh edges,
     # where the edge coordinates are in dimensionful units.
-    x, y = xi * mesh.edge_mesh.centers.T
+    edge_centers = xi * mesh.edge_mesh.centers
+    edge_xs, edge_ys = edge_centers.T
     Bc2 = device.Bc2
-    z = device.layer.z0 * xi * np.ones_like(x)
+    z0 = device.layer.z0 * np.ones_like(edge_xs)
     J_scale = 4 * ((ureg(current_units) / length_units) / K0).to_base_units()
     assert "dimensionless" in str(J_scale.units), str(J_scale.units)
     J_scale = J_scale.magnitude
@@ -275,9 +278,9 @@ def solve(
         .magnitude
     )
     A_kwargs = dict(t=0) if time_dependent_vector_potential else dict()
-    vector_potential = applied_vector_potential_(x, y, z, **A_kwargs)
+    vector_potential = applied_vector_potential_(edge_xs, edge_ys, z0, **A_kwargs)
     vector_potential = np.asarray(vector_potential)[:, :2]
-    if vector_potential.shape != x.shape + (2,):
+    if vector_potential.shape != edge_xs.shape + (2,):
         raise ValueError(
             f"Unexpected shape for vector_potential: {vector_potential.shape}."
         )
@@ -336,7 +339,7 @@ def solve(
     use_pardiso = options.sparse_solver is SparseSolver.PARDISO
     if use_pardiso:
         assert mu_laplacian_lu is None
-        import pypardiso
+        import pypardiso  # type: ignore
 
         mu_laplacian = operators.mu_laplacian
     # Initialize the order parameter and electric potential
@@ -354,9 +357,8 @@ def solve(
         raise ValueError("The disorder parameter epsilon must be in range [-1, 1].")
 
     if options.include_screening:
-        A_scale = (ureg("mu_0") / (4 * np.pi) * K0 / Bc2).to_base_units().magnitude
-        areas = A_scale * mesh.areas
-        edge_centers = mesh.edge_mesh.centers
+        A_scale = (ureg("mu_0") / (4 * np.pi) * K0 / A0).to(1 / length_units)
+        areas = A_scale.magnitude * mesh.areas * xi**2
         if not options.screening_use_numba:
             # Pre-compute the kernel for the screening integral.
             inv_rho = A_scale / distance.cdist(
@@ -395,7 +397,7 @@ def solve(
     ):
         A_induced = induced_vector_potential
         A_applied = applied_vector_potential
-        nonlocal tentative_dt
+        nonlocal tentative_dt, vector_potential
         step = state["step"]
         time = state["time"]
         old_sq_psi = np.abs(psi) ** 2
@@ -410,12 +412,12 @@ def solve(
 
         dA_dt = 0.0
         if time_dependent_vector_potential:
-            vector_potential = applied_vector_potential_(x, y, z, t=time)[:, :2]
-            vector_potential *= vector_potential_scale
+            vector_potential = applied_vector_potential_(edge_xs, edge_ys, z0, t=time)
+            vector_potential = vector_potential_scale * vector_potential[:, :2]
             dA_dt = np.einsum(
                 "ij, ij -> i",
                 (vector_potential - A_applied) / dt,
-                edge_directions,
+                normalized_directions,
             )
             if not options.include_screening:
                 if np.any(np.abs(dA_dt) > 0):
@@ -445,7 +447,7 @@ def solve(
                     dA_dt = np.einsum(
                         "ij, ij -> i",
                         (vector_potential + A_induced - A_applied) / dt,
-                        edge_directions,
+                        normalized_directions,
                     )
 
             # Adjust the time step and calculate the new the order parameter
@@ -466,7 +468,7 @@ def solve(
             )
             # Compute the supercurrent, scalar potential, and normal current
             supercurrent = get_supercurrent(psi, operators.psi_gradient, edges)
-            rhs = divergence @ (supercurrent - dA_dt) - (
+            rhs = (divergence @ (supercurrent - dA_dt)) - (
                 mu_boundary_laplacian @ mu_boundary
             )
             if use_pardiso: